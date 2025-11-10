77416652161e8939c109cb2c5ccc9eb1b4ea2e03
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-11-10 21:23:01 +0600

    Add files via upload

---
+++ CharAtIndex.java
@@ -0,0 +1,8 @@
+public class CharAtIndex {
+    public static void main(String[] args) {
+        String str = "Programming";
+        int index = 4;
+        char ch = str.charAt(index);
+        System.out.println("Character at index " + index + " is: " + ch);
+    }
+}
